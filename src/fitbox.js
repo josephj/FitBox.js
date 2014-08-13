428c25b01eb6494c31d35905ca9658a3fa3f7630
josephj <josephj6802@gmail.com>
2014-08-14 08:27:09 +1000

    Use scrollWidth to detect text width

--- src/fitbox.js
+++ src/fitbox.js
@@ -5,16 +5,8 @@
     'use strict';
 
     /**
-     * Adjusts the font size automatically according to its container's width and hiehgt
-     *
-     * new FitBox(container, {
-     *     minFontSize: '50px', // Default null
-     *     maxFontSize: '100px', // Default null
-     *     adjustAfterWindowResize: false, // Default false
-     *     truncateStyle: 'ellipsis'
-     * });
-     *
-     * OR...
+     * Adjusts the font size automatically according to
+     * its container's width and hiehgt.
      *
      * $(container).fitbox('option', {
      *     minFontSize: '50px',
@@ -24,29 +16,59 @@
      * });
      *
      * @constructor FitBox
+     * @param container {jQuery|HTMLElement|String} The container
+     * @param attrs {Object} The config object.
      */
     function FitBox(container, attrs) {
-        var that = this;
-        that.container = $(container);
-        that.clone = null;
+        var that = this,
+            $window = $(window);
+        container = $(container);
         attrs = attrs || {};
+
+        // Init properties
+        that._needTruncate = false;
+        that._originalFontSize = parseInt(container.css('font-size'), 10);
+        that.adjustAfterWindowResize = (attrs.adjustAfterWindowResize === true) ? true : false;
+        that.container = container;
         that.debug = (attrs.debug) ? true : false;
-        that.init(attrs);
-        that.bind();
-        that.render(that.container);
+        that.debug = true;
+        that.ellipsisSelector = attrs.ellipsisSelector || null;
+        that.maxFontSize = parseInt(attrs.maxFontSize, 10) || null;
+        that.maxTryAmount = parseInt(attrs.maxTryAmount, 10) || 300;
+        that.minFontSize = parseInt(attrs.minFontSize, 10) || null;
+
+        // Bind resize event when adjustAfterWindowResize is true
+        if (that.adjustAfterWindowResize) {
+            $window.resize(function() {
+                if (that.resizeTimer) {
+                    clearTimeout(that.resizeTimer);
+                }
+                that.resizeTimer = setTimeout(function() {
+                    $window.trigger('resizeEnd');
+                }, 1000);
+            });
+            $window.on('resizeEnd', function () {that.sync();});
+        }
+
+        // Find best fit font size and apply it
+        that.sync();
     }
 
-    FitBox.MAX_TRY_AMOUNT = 300;
-
     FitBox.prototype = {
-        log: function (msg, type) {
+        /**
+         * Show debugging logs when debug:true
+         */
+        _log: function (msg, type) {
             var that = this;
-            if (!that.debug) {
+            if (!that.debug || !window.console) {
                 return;
             }
             type = type || 'info';
             console[type]('[FitBox] ' + msg);
         },
+        /**
+         * Find beginning font size
+         */
         _calculateRatio: function (ratio, length) {
             ratio = Math.log(ratio);
             ratio += 1;
@@ -56,101 +78,103 @@
          * Get appropriate font size by cloning an invisible container
          * and change its font size by loop. Find the closest height.
          *
-         * NOTE: This method ignores both the maxFontSize and minFontSize
-         *       which user might set.
+         * Note that it ignores both the maxFontSize and minFontSize
+         * which user might set.
          */
-        getBestFontSize: function () {
+        _getBestFontSize: function () {
             var that = this,
-                $host = that.container,
-                $proxy = $host.clone(),
-                counter = 0,
-                fontSize = parseInt($host.css('font-size'), 10),
-                originalFontSize = fontSize,
-                hostHeight,  // Containter height
-                proxyHeight, // Clone height
-                proxyWidth,
-                ratio;
+                $host,       // Host = container
+                $proxy,      // Proxy = clonned container
+                ratio,       // Host width and height ratio (fixed)
+                hostWidth,   // Host width (fixed)
+                hostHeight,  // Host height (fixed)
+                counter,
+                isAdd,
+                fontSize,    // Current font size
+                proxyWidth,  // Current proxy width
+                proxyHeight, // Current proxy height
+                step;
 
-            that.log('getBestFontSize() is executed');
+            that._log('_getBestFontSize() is executed');
 
-            // Clone container to an invisible element for testing different font sizes.
-            $proxy.addClass('fitbox-proxy');
+            // Clone container as an invisible div for testing
+            // different font sizes
+            $host = that.container;
+            $proxy = $host.clone();
             $host.parent().append($proxy);
             $proxy.css({
-                'background-color': 'yellow',
+                'height'   : 'auto',
                 'width'    : $host.width() + 'px',
                 'overflow' : 'hidden',
-                'height'   : 'auto',
                 'position' : 'absolute',
-                'left'     : '0',
-                'bottom'   : 'auto'
+                'left'     : '-1000em',
+                'top'      : '-1000em'
             });
 
-            that.log([
-                '',
-                'hostWidth = '+ $host.width(),
-                'hostHeight = '+ $host.height(),
-                'proxyWidth = '+ $proxy.width(),
-                'proxyHeight = '+ $proxy.height()
-            ].join('\n'));
-
-            // Initial height.
+            // Initial values
+            hostWidth = parseInt($host.width(), 10);
             hostHeight = parseInt($host.height(), 10);
-            proxyWidth = parseInt($proxy.width(), 10);
+            proxyWidth = parseInt($proxy[0].scrollWidth, 10);
             proxyHeight = parseInt($proxy.height(), 10);
-            that.log('getBestFontSize() - proxyHeight:hostHeight = ' + proxyHeight + ':' + hostHeight);
 
-            // Don't do anything if it's an empty block.
-            if (proxyWidth === 0 || proxyHeight === 0 || $.trim($proxy.html()).length === 0) {
-                that.log('No text or height equals to 0');
+            // Don't do anything if it's an empty block
+            fontSize = parseInt($host.css('font-size'), 10);
+            if (!proxyWidth || !proxyHeight || $.trim($proxy.html()).length === 0) {
+                that._log('>> No text or height equals to 0', 'warn');
                 return fontSize;
             }
 
+            // Set initial font size
+            // TODO - Find a better way to calculate
             ratio = that._calculateRatio(hostHeight / proxyHeight);
             fontSize = fontSize * ratio;
             $proxy.css('font-size', fontSize + 'px');
-            proxyHeight = $proxy.height();
+            proxyWidth = parseInt($proxy[0].scrollWidth, 10);
+            proxyHeight = parseInt($proxy.height(), 10);
 
-            if (proxyHeight > hostHeight) {
-                that.log('getBestFontSize() - should DECREASE font size');
-                do {
-                    fontSize = parseInt($proxy.css('font-size'), 10) - 1;
-                    $proxy.css('font-size', fontSize + 'px');
-                    that.log('getBestFontSize() - try ' + fontSize + 'px');
-                    proxyHeight = $proxy.height();
-                    counter += 1;
-                    if (counter > FitBox.MAX_TRY_AMOUNT) {
-                        that.log('getBestFontSize() - Exceed legal detecting amount. Give up!', 'warn');
-                        $proxy.remove();
-                        $proxy = null;
-                        return originalFontSize;
-                    }
-                } while (proxyHeight > hostHeight);
-                fontSize -= 1;
-            } else if (proxyHeight < hostHeight) {
-                that.log('getBestFontSize() - should INCREASE font size');
-                do {
-                    fontSize = parseInt($proxy.css('font-size'), 10) + 1;
-                    $proxy.css('font-size', fontSize + 'px');
-                    that.log('getBestFontSize() - try ' + fontSize + 'px');
-                    proxyHeight = $proxy.height();
-                    counter += 1;
-                    if (counter > FitBox.MAX_TRY_AMOUNT) {
-                        that.log('getBestFontSize() - Exceed legal detecting amount. Give up!', 'warn');
-                        $proxy.remove();
-                        $proxy = null;
-                        return originalFontSize;
-                    }
-                } while (proxyHeight < hostHeight);
-                fontSize -= 1;
+            if (proxyHeight > hostHeight || proxyWidth > hostWidth) { // Decrease
+                isAdd = false;
+                step = -1;
+            } else if (proxyHeight < hostHeight && proxyWidth <= hostWidth) { // Increase
+                isAdd = true;
+                step = 1;
+            } else {
+                return fontSize; // Already the best!
             }
 
-            // Destroy proxy because we got the best font size.
+            while (that._shouldContinue(isAdd, [hostWidth, hostHeight], [proxyWidth, proxyHeight])) {
+                // Change font size
+                fontSize = parseInt($proxy.css('font-size'), 10) + step;
+                $proxy.css('font-size', fontSize + 'px');
+                that._log('>> Try ' + fontSize + 'px');
+                // Get new width and height
+                proxyHeight = parseInt($proxy.height(), 10);
+                proxyWidth = parseInt($proxy[0].scrollWidth, 10);
+                // Check if the amount of iterations is more than maximum try limit
+                counter += 1;
+                if (counter > that.maxTryAmount) {
+                    that._log('>> Exceed legal detecting amount. Give up!', 'warn');
+                    $proxy.remove();
+                    $proxy = null;
+                    return false;
+                }
+            }
+
+            // Destroy proxy after getting best font size.
             $proxy.remove();
             $proxy = null;
 
-            that.log('getBsetFontSize() - best font size is ' + fontSize);
+            that._log('>> Best font size is ' + fontSize);
             return fontSize;
+        },
+        _shouldContinue: function (isAdd, hostSize, proxySize) {
+            var result;
+            if (isAdd) { // Increase
+                result = (proxySize[1] < hostSize[1] && proxySize[0] <= hostSize[0]);
+            } else { // Decrease
+                result = (proxySize[1] > hostSize[1] || proxySize[0] > hostSize[0]);
+            }
+            return result;
         },
         /**
          * Set appropriate font sie according to minFontSize and maxFontSize
@@ -163,41 +187,34 @@
                 currentFontSize = parseInt(that.container.css('font-size'), 10),
                 newFontSize;
 
-            that.log('setFontSize() is execute');
+            that._log('setFontSize() is execute');
 
             if (that.maxFontSize && fontSize > that.maxFontSize) {
                 newFontSize = that.maxFontSize;
-                that.log('setFontSize() - You better make the alignment to middle...');
+                that._log('>> You better make the alignment to middle...');
             } else if (that.minFontSize && fontSize < that.minFontSize) {
                 newFontSize = that.minFontSize;
                 that.needTruncate = true;
-                that.log('setFontSize() - You better do the truncation...');
+                that._log('>> You better do the truncation...');
             } else {
                 newFontSize = fontSize;
             }
 
-            that.log('setFontSize() - Initial font size is ' + currentFontSize + 'px');
+            that._log('>> Initial font size is ' + currentFontSize + 'px');
             if (newFontSize !== currentFontSize) {
                 that.container.css('font-size', newFontSize + 'px');
-                that.log('setFontSize() - New font size is ' + newFontSize + 'px');
+                that._log('>> New font size is ' + newFontSize + 'px');
                 // Trigger a `fitbox:change` custom event.
                 $(document).trigger('fitbox:change', [currentFontSize, newFontSize, that.needTruncate]);
             }
             if (that.needTruncate) {
                 that.truncate();
             }
-            that.log('========================');
-        },
-        sync: function () {
-            var that = this,
-                bestFontSize;
-            bestFontSize = that.getBestFontSize();
-            that.setFontSize(bestFontSize);
         },
         truncate: function () {
             var that = this,
                 $wrapper;
-            that.log('truncate() is executed');
+            that._log('truncate() is executed');
             if (!that.container.dotdotdot) {
                 console.warn('You need to install the dotdotdot jquery plugin');
                 return;
@@ -205,53 +222,44 @@
             $wrapper = (that.ellipsisSelector) ? that.container.find(that.ellipsisSelector) : that.container;
             $wrapper.dotdotdot();
         },
-        init: function (attrs) {
-            var that = this;
-            that.log('init() is executed');
-            that.needTruncate = false;
-            that.ellipsisSelector = attrs.ellipsisSelector || null;
-            that.minFontSize = parseInt(attrs.minFontSize, 10) || null;
-            that.maxFontSize = parseInt(attrs.maxFontSize, 10) || null;
-            that.adjustAfterWindowResize = (attrs.adjustAfterWindowResize === true) ? true : false;
-        },
-        bind: function () {
-            var that = this;
-            that.log('bind() is executed');
-
-            if (!that.adjustAfterWindowResize) {
-                return false;
-            }
-
-            // Trigger font size change only for once.
-            $(window).resize(function() {
-                if (that.resizeTimer) {
-                    clearTimeout(that.resizeTimer);
-                }
-                that.resizeTimer = setTimeout(function() {
-                    $(window).trigger('resizeEnd');
-                }, 1000);
-            });
-
-            $(window).on('resizeEnd', function () {that.sync();});
-        },
-        render: function (container) {
-            var that = this;
-            that.log('render() is executed');
-            that.sync();
+        /**
+         * Find best font size and apply it
+         *
+         * @method sync.
+         */
+        sync: function () {
+            var that = this,
+                bestFontSize;
+            bestFontSize = that._getBestFontSize();
+            that.setFontSize(bestFontSize);
         }
     };
 
     // Make a jquery-like usage
-    $.fn.fitbox = function (options) {
-        $.each(this, function () {
+    $.fn.fitbox = function (option, value) {
+        this.each(function () {
             var $el = $(this),
-                fitbox = $el.data('fitbox');
-            if (!fitbox) {
-                fitbox = new FitBox($el, options);
-                $el.data('fitbox', fitbox);
+                attrs,
+                instance;
+
+            // Get config object
+            attrs = ($.isPlainObject(option)) ? option : {};
+            attrs = $.extend($el.data(), attrs); // Accept data attributes
+
+            // Render the fitbox
+            instance = $el.data('fitbox');
+            if (!instance) {
+                instance = new FitBox($el, option);
+                $el.data('fitbox', instance);
+            }
+
+            // Allow user to call public method via JavaScript
+            if (typeof option === 'string' && instance[option]) {
+                instance[option].call(instance, value);
             }
         });
     };
+    $.fn.fitbox.Constructor = FitBox;
 
     // Promote to global
     window.FitBox = FitBox;
